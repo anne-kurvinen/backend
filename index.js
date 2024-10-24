65e62ed0c4df394b424f08d38fc50e03c0d1075a
Anne Kurvinen <kurvinen@live.se>
2024-10-24 16:14:45 +0200

    cors added

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ const express = require('express')
 const app = express(),
   port = process.env.PORT || 3000
 
-app.get('/', (_request, response) => {
+  app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
+   
+  app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80')
+  })
+
+/* app.get('/', (_request, response) => {
   response.send({ hello: 'World' })
 })
 
 app.listen(port, () => {
   console.log(`Redo på http://localhost:${port}/`)
-})
+}) */
+
+/* var express = require('express')
+var cors = require('cors')
+var app = express()
+ 
+app.use(cors()) */
+ 
